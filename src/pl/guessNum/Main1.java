99b5f0b4669316f6a4d703bc1a5cd7d3a8d67ce9
BasiaM1 <b.murzynowska@wp.pl>
2019-01-06 14:25:28 +0100

    Initial commit -m "Guess Number! done"

---
+++ src/pl/guessNum/Main1.java
@@ -0,0 +1,41 @@
+package pl.guessNum;
+
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main1 {
+
+    public static void main(String[] args) {
+
+guessNum();
+    }
+
+
+    static void guessNum() {
+        Random r = new Random();
+        int rand = r.nextInt(101);
+        System.out.println(rand);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Guess the number: ");
+        while (true) {
+            while (!sc.hasNextInt()) {
+                sc.next();
+                System.out.println("This is not the number, please input once again");
+            }
+            int number = sc.nextInt();
+            if (number < rand) {
+                System.out.println("Too little");
+            } else if (number > rand) {
+                System.out.println("Too much!");
+            } else if (number == rand) {
+                System.out.println("Zgadles");
+                break;
+            }
+        }
+        sc.close();
+    }
+
+}
+
